a5d2c9beb3adaf9f353c59c82a146aff10b5e80a
Cloey <chamclo@gmail.com>
2018-03-06 10:23:32 -0500

    first commit

---
+++ app/models/location.rb
@@ -0,0 +1,13 @@
+class Location < ApplicationRecord
+	geocoded_by :my_location
+	after_validation :geocode
+
+	validates :address, presence: :true
+	validates :city, presence: :true
+
+	def my_location
+		"#{address}, #{city}, GA"
+	end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :locations
+
+  root 'locations#new'
+
+  resources :locations
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
